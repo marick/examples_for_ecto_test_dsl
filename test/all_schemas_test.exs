fccdcb650c71b8ca1fa6d6e61a58e5d4603fd08b
Brian Marick <marick@exampler.com>
2020-10-10 17:37:11 -0500

    Turn all_schemas_test into a script.

--- test/all_schemas_test.exs
+++ test/all_schemas_test.exs
@@ -1,10 +1,20 @@
 defmodule App.Schemas.AllSchemasTest do
   use ExUnit.Case, async: true
   import TransformerTestSupport.Macros
-  alias Examples.Schemas
 
-  check_examples_with(Schemas.Basic.Simplest.Tester)
-  check_examples_with(Schemas.Basic.Validation.Tester)
-  check_examples_with(Schemas.Basic.Like.Tester)
+  @module_name_regex ~r/^.*defmodule[\s]+([.\w]+)/
+  
+  for path <- Path.wildcard("test/*example.ex") do
+    first_line = File.open!(path, [:read]) |> IO.read(:line)
+
+    case Regex.run(@module_name_regex, first_line) do
+      [_all, module_name] ->
+        module_name 
+        |> Module.safe_concat("Tester")
+        |> check_examples_with
+      _ ->
+        raise ~s/Could not find the module name inside file "#{path}"/
+    end
+  end
 end
 
