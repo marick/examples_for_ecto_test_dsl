33d20243ca50351a4e2dc9e473330df8509a4c37
Brian Marick <marick@exampler.com>
2020-10-26 14:01:03 -0500

    Demonstrate check_examples_with in diff. file.

--- test/20_basic_validation_test.exs
+++ test/20_basic_validation_test.exs
@@ -1,9 +1,17 @@
 defmodule App.Schemas.Basic.ValidationTest do
-  use App.Case
+  use ExUnit.Case, async: true
   alias Examples.Schemas.Basic.Validation.Tester
 
   test "first version" do 
     Tester.validate(:ok)
     Tester.validate(:bad_date)
   end
+
+  # To check the examples in a single module:
+  # This will generate one ExUnit `test` for each example.
+
+  alias Examples.Schemas.Basic.Validation, as: Basic
+  import TransformerTestSupport.Runner
+  
+  check_examples_with(Basic.Tester)
 end
--- test/all_schemas_test.exs
+++ test/all_schemas_test.exs
@@ -1,18 +1,10 @@
 defmodule App.Schemas.AllSchemasTest do
   use ExUnit.Case, async: true
-  import TransformerTestSupport.Tester
+  import TransformerTestSupport.Runner
 
   # To check a bunch of examples in a bunch of files.
   # This will generate one ExUnit `test` for each example.
 
   check_examples_in_files("test/*example.ex")
-
-  # To check the examples in a single module:
-
-  # check_examples_with(Examples.Schemas.Basic.Validation.Tester)
-
-  # The above can't be uncompiled because it generates tests with
-  # the same names as some of the `check_examples_in_files` tests.
-  
 end
 
